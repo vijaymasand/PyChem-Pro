06a90998618085bf07498ca594796c683e8d646b
Vijay Masand <vijaymasand@gmail.com>
2026-06-05 14:04:57 +0530

    Remove mordred-develop folder

--- mordred-develop/examples/article/Code2.py
+++
@@ -1,9 +0,0 @@
-from rdkit import Chem
-
-from mordred.RingCount import RingCount
-
-# Start Code 2
-rings = RingCount()
-rings6 = RingCount(order=6)
-print(rings(Chem.MolFromSmiles("c1ccccc1")), rings(Chem.MolFromSmiles("C1CCCCCC1")))
-print(rings6(Chem.MolFromSmiles("c1ccccc1")), rings6(Chem.MolFromSmiles("C1CCCCCC1")))
